a93933b7d09cb22ef40b4d8b28f0184070baeb05
Iris Nogueira <ires.janine@hotmail.com>
2025-07-12 20:55:23 -0300

    ajusta atribuição de cores no algoritmo guloso

--- greedy.py
+++ greedy.py
@@ -1,24 +1,25 @@
 
 def coloring(grafo):
     print("[Greedy] Coloração iniciada...")
+    grafo.resetar_cores()
 
-    disponibilidadeCor = [-1 for i in range(0, grafo.n + 1)]
+    disponibilidadeCor = [-1 for i in range(1, grafo.n + 1)]
 
-    grafo.vertices[1].cor = 0
+    grafo.vertices[1].cor = 1
     for i in range(2, grafo.n + 1):
 
         for v in grafo.vertices[i].vizinhanca:
             if (grafo.vertices[v].cor is not None):
                 disponibilidadeCor[grafo.vertices[v].cor] = 1
 
-        cor = 0
+        cor = 1
 
-        while cor < grafo.n:
+        while cor < grafo.n + 1:
             if (disponibilidadeCor[cor] == -1):
                 break
             cor += 1
 
-        grafo.vertices[v].cor = cor
+        grafo.vertices[i].cor = cor
 
         for v in grafo.vertices[i].vizinhanca:
             if (grafo.vertices[v].cor is not None):
