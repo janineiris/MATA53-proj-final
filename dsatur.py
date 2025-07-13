6ac45af956dd44345cfa906273729e6d7df5c42c
Rafael Nagy <rafaelcnagy@hotmail.com>
2025-07-13 14:44:40 -0300

    Otimiza algoritmo DSatur com estruturas de dados de menor complexidade

--- dsatur.py
+++ dsatur.py
@@ -1,51 +1,59 @@
-def coloring(grafo):
-    def atualiza_vizinhos(grafo, vertice: int, cor: int) -> None:
-        for vizinho in grafo.vertices[vertice].vizinhanca:
-            cores_usadas[vizinho].add(cor)
-            saturacao[vizinho] = len(cores_usadas[vizinho])
+import heapq
 
+
+def coloring(grafo):
     def menor_cor_possivel(grafo, vertice: int) -> int:
-        vizinhos = grafo.vertices[vertice].vizinhanca
-        cores_vizinhos = {
-            grafo.vertices[v].cor for v in vizinhos if grafo.vertices[v].cor is not None
-        }
-        cor = 0
-        while cor in cores_vizinhos:
+        for vizinho in grafo.vertices[proximo].vizinhanca:
+            cor_vizinho = grafo.vertices[vizinho].cor
+            if cor_vizinho is not None:
+                cores_usadas[cor_vizinho] = True
+
+        cor = 1
+        while cor < grafo.n + 1:
+            if (cores_usadas[cor] == False):
+                break
             cor += 1
+
+        for vizinho in grafo.vertices[proximo].vizinhanca:
+            cor_vizinho = grafo.vertices[vizinho].cor
+            if cor_vizinho is not None:
+                cores_usadas[cor_vizinho] = False
+
         return cor
 
     print("\n[DSATUR] Coloração iniciada...")
     grafo.resetar_cores()
 
     # Inicializa saturação de todos os vértices como 0
-    saturacao = {v: 0 for v in grafo.vertices}
-    cores_usadas = {v: set() for v in grafo.vertices}
-    nao_coloridos = set(grafo.vertices.keys())
+    saturacao = {v: 0 for v in grafo.vertices.keys()}
+    grau = {index: len(vertice.vizinhanca) for index, vertice in grafo.vertices.items()}
+    cores_vizinhos = {v: set() for v in grafo.vertices.keys()}
 
-    # Começa pelo vértice de maior grau
-    inicial = max(nao_coloridos, key=lambda v: len(grafo.vertices[v].vizinhanca))
-    grafo.vertices[inicial].cor = 0
-    nao_coloridos.remove(inicial)
+    cores_usadas = [False for i in range(0, grafo.n + 1)]
 
-    # Atualiza saturação dos vizinhos
-    atualiza_vizinhos(grafo, inicial, 0)
+    fila_prioridade = []
+    for vertice in grafo.vertices:
+        heapq.heappush(fila_prioridade,  ((-saturacao[vertice], -grau[vertice], vertice), vertice))
 
-    # Continua enquanto houver vértices não coloridos
-    while nao_coloridos:
-        # Escolhe o vértice com maior saturação (desempata pelo grau)
-        proximo = max(
-            nao_coloridos,
-            key=lambda v: (saturacao[v], len(grafo.vertices[v].vizinhanca))
-        )
+    while fila_prioridade:
+        _, proximo = heapq.heappop(fila_prioridade)
+
+        # Se o próximo da fila tiver saturação igual (desatualizada), recalcula e reempilha com valor atualizado
+        while fila_prioridade and _ == fila_prioridade[0][0]:
+            _, vertice = heapq.heappop(fila_prioridade)
+            heapq.heappush(fila_prioridade,  ((-saturacao[vertice], -grau[vertice], vertice), vertice))
 
         # Determina a menor cor possível que os vizinhos ainda não usam
         cor = menor_cor_possivel(grafo, proximo)
 
         # Atribui cor
         grafo.vertices[proximo].cor = cor
-        nao_coloridos.remove(proximo)
 
-        # Atualiza saturação dos vizinhos do vértice recém-colorido
-        atualiza_vizinhos(grafo, proximo, cor)
+        for vizinho in grafo.vertices[proximo].vizinhanca:
+            # Adiciona vizinhos na fila de prioridade
+            if grafo.vertices[vizinho].cor is None:
+                heapq.heappush(fila_prioridade, ((-saturacao[vizinho], -grau[vizinho], vizinho), vizinho))
+                cores_vizinhos[vizinho].add(cor)
+                grau[vizinho] -= 1
 
     print("[DSATUR] Coloração finalizada.")
